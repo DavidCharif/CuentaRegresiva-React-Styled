c8f00835aa13cec962fe6d71d1a6df00d6569a70
David Charif <53876827+DavidCharif@users.noreply.github.com>
2022-02-13 12:31:41 -0500

    Add files via upload

---
+++ reto-1/src/componentes/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Header as HeaderStyled } from '../../styles/style'
+
+
+const Header = () => {
+
+  
+  return (
+    <>
+    
+      <HeaderStyled/>
+      </>
+  )
+  
+}
+
+export default Header
